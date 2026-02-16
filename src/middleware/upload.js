68839c8514b1fb7eeac91902edef79a74283ca34
Sanjid Rahman <sanjid.rahman@aikyro.com>
2026-02-16 23:17:42 +0530

    fix: :sparkles: add check if uploads exists if not create one, and update .gitignore

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,5 +1,10 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+
+if (!fs.existsSync('uploads')) {
+  fs.mkdirSync('uploads', { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
